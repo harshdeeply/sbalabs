04a37001580d2bce2518cce8d76ed150dba33aa4
harshdeeply <48420037+harshdeeply@users.noreply.github.com>
2020-11-20 20:29:21 -0800

    Update app.py

--- processing/app.py
+++ processing/app.py
@@ -9,10 +9,19 @@ import datetime
 import yaml
 import requests
 
-with open("app_conf.yaml", "r") as f:
+if "TARGET_ENV" in os.environ and os.environ["TARGET_ENV"] == "test":
+    print("In Test Environment")
+    app_conf_file = "/config/app_conf.yml"
+    log_conf_file = "/config/log_conf.yml"
+else:
+    print("In Dev Environment")
+    app_conf_file = "app_conf.yml"
+    log_conf_file = "log_conf.yml"
+
+with open(app_conf_file, "r") as f:
     app_config = yaml.safe_load(f.read())
 
-with open("log_conf.yml", "r") as f:
+with open(log_conf_file, "r") as f:
     log_config = yaml.safe_load(f.read())
     logging.config.dictConfig(log_config)
 
@@ -39,6 +48,8 @@ def initJsonFile():
 
 logger = logging.getLogger("basicLogger")
 
+logger.info("App Conf File: %s" % app_conf_file)
+logger.info("Log Conf File: %s" % log_conf_file)
 
 def get_stats():
     """ Gets stats """
